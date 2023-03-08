546019bed44d09bcf34be0ee7204615d4f2bb24f
ravilella1234 <masters.selenium@gmail.com>
2023-03-08 20:01:48 +0530

    Selenium BrowserLaunchings

---
+++ March2023Selenium7PMBatch/src/test/java/seleniumpack/BrowserLaunch.java
@@ -0,0 +1,14 @@
+package seleniumpack;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserLaunch 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		ChromeDriver driver = new ChromeDriver();
+	}
+
+}
